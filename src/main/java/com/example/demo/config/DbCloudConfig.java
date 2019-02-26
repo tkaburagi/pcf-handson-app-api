d9d3d66275e83434bcb9a1aeede45e43ea4fd46f
tkaburagi <tkaburagi@pivotal.io>
2019-02-26 23:47:44 +0900

    added ci

--- src/main/java/com/example/demo/config/DbCloudConfig.java
+++ src/main/java/com/example/demo/config/DbCloudConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import javax.sql.DataSource;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -24,6 +26,10 @@ public class DbCloudConfig extends AbstractCloudConfig {
         databasePopulator.addScript(new ClassPathResource("demo.sql"));
 
         DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+
+//        PooledServiceConnectorConfig.PoolConfig poolConfig = new PooledServiceConnectorConfig.PoolConfig(
+//                5 /* min */, 30 /* max */, 3000 /* maxWait */);
+//        return connectionFactory().dataSource(new DataSourceConfig(poolConfig, null));
 
         return dataSource;
     }
