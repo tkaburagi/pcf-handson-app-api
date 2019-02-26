e7445053e8957ade6ba2e74bc133d3938d5f4b14
tkaburagi <tkaburagi@pivotal.io>
2019-02-27 02:29:59 +0900

    v2

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -39,7 +39,7 @@ public class ApiController {
 
         log.info("Handling home");      
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("message","Helloworld V1");
+        body.put("message","Helloworld V2");
         body.put("index", System.getenv("CF_INSTANCE_INDEX"));
         body.put("host", System.getenv("CF_INSTANCE_IP"));
         body.put("java", System.getProperty("java.vm.version"));
